01ddb41387ea0d171cada17b99f7d2272bce7891
Great Lucky <lgreat098@gmail.com>
2025-11-11 09:12:52 +0100

    fetch data from google search api

--- app/search/web/page.tsx
+++ app/search/web/page.tsx
@@ -1,3 +1,24 @@
-export default function page() {
-  return <div>webSearchPage</div>;
+interface Results {
+  title: string;
+}
+
+export default async function page({
+  searchParams,
+}: {
+  searchParams: Promise<{ query: string }>;
+}) {
+  const query = (await searchParams).query;
+
+  const res = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${query}`
+  );
+  const data = await res.json();
+  const results: Results[] = data.items;
+
+  return (
+    <>
+      {results &&
+        results.map((result) => <h1 key={result.title}>{result.title}</h1>)}
+    </>
+  );
 }
